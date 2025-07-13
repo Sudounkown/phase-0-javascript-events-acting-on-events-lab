12e35b9659fd4922e359191d9eaab5b69f400ea0
Roy Koigu <roykoigu@protonmail.com>
2025-07-13 13:06:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+const dodger = document.getElementById("dodger")
+
+dodger.style.backgroundColor = "#0000FF"
+dodger.style.bottom = "0px"
+dodger.style.right = "0px"
+
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+  else if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+});
+
+const moveDodgerLeft = () => {
+
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+
+}}
+
+
+
+const moveDodgerRight = () => {
+
+ const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+}}
